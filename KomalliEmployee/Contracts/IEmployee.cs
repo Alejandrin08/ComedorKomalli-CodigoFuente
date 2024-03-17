52dd560e357c9563148cea90f2ddf0a6e67d3225
alejandrin09 <zs21013870@estudiantes.uv.mx>
2024-03-17 16:45:54 -0600

    Creación de las páginas del login y el cambio de contraseña. Se agrego logger. Se agrego una clase para usar el patrón de diseño Singleton. Se agrego los métodos para usar el procedimiento almacenado y hacer la validación del login así como para cambiar contraseña y el estado lógico del usuario. Se añadio una carpeta para realizar las validaciones. Se añadio imagenes y un gif. Cambios a realizar: cambiar el diseño de las páginas, revisar las buenas prácticas de codificación.

---
+++ KomalliEmployee/Contracts/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using KomalliEmployee.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomalliEmployee.Contracts {
+    internal interface IEmployee {
+
+        public Dictionary<UserRole, int> ValidateUser(string email, string password);
+        public int UpdatePassword(string email, string password);
+    }
+}
---
+++ KomalliEmployee/Model/SingletonClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomalliEmployee.Model {
+    internal class SingletonClass {
+
+        private static SingletonClass _instance;
+        public static SingletonClass Instance {
+            get {
+                if (_instance == null) {
+                    _instance = new SingletonClass();
+                }
+                return _instance;
+            }
+        }
+
+        public string Email { get; set; }
+    }
+}
