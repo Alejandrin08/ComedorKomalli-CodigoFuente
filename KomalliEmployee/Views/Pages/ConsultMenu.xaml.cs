e349da741c5b2d74a3d76f1dad861dc6c9c863b6
alejandrin09 <zs21013870@estudiantes.uv.mx>
2024-05-01 19:18:12 -0600

    Se agrego el caso de uso 26.

---
+++ KomalliEmployee/Views/Pages/ConsultMenu.xaml.cs
@@ -0,0 +1,65 @@
+﻿using KomalliEmployee.Controller;
+using KomalliEmployee.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Data.Entity.Infrastructure.Design.Executor;
+
+namespace KomalliEmployee.Views.Pages {
+    /// <summary>
+    /// Lógica de interacción para ConsultMenu.xaml
+    /// </summary>
+    public partial class ConsultMenu : Page {
+
+        private MenuController _menuController;
+        public ConsultMenu() {
+            InitializeComponent();
+
+            _menuController = new MenuController();
+
+            MenuModel menuBreakfast = _menuController.GetBreakfastOfTheDay(DateTime.Today);
+            if (menuBreakfast != null) {
+                lstBreakfast.ItemsSource = new List<MenuModel> { menuBreakfast };
+            } else {
+                App.ShowMessageError("No se pudo recuperar el menú del desayuno", "Error");
+            }
+
+            MenuModel menuFood = _menuController.GetFoodOfTheDay(DateTime.Today);
+            if (menuFood != null) {
+                lstFood.ItemsSource = new List<MenuModel> { menuFood };
+            } else {
+                App.ShowMessageError("No se pudo recuperar el menú de comida", "Error");
+            }
+
+            dtpSelectDate.SetValue(DatePicker.SelectedDateProperty, DateTime.Today);
+        }
+
+        private void SelectedDateChangedConsultMenu(object sender, SelectionChangedEventArgs e) {
+            DateTime selectedDate = (DateTime)dtpSelectDate.SelectedDate;
+            MenuModel menuBreakfast = _menuController.GetBreakfastOfTheDay(selectedDate);
+            if (menuBreakfast != null) {
+                lstBreakfast.ItemsSource = new List<MenuModel> { menuBreakfast };
+            } else {
+                App.ShowMessageWarning("No se pudo recuperar el menú del desayuno", "Error");
+            }
+
+            MenuModel menuFood = _menuController.GetFoodOfTheDay(selectedDate);
+            if (menuFood != null) {
+                lstFood.ItemsSource = new List<MenuModel> { menuFood };
+            } else {
+                App.ShowMessageWarning("No se pudo recuperar el menú de comida", "Error");
+            }
+        }
+    }
+}
